af8413daf766daccabe122a09c24080acee49c5d
B3Max <B3dMaxOsp@yandex.ru>
2023-02-01 13:38:14 +0300

    cheat

--- admin/add_user.php
+++ admin/add_user.php
@@ -2,7 +2,6 @@
 	// include('session.php');
 	include('../conn.php');
 	if(isset($_POST['adduser'])){
-		echo 1234;
 		$name=$_POST['name'];
 		$username=$_POST['username'];
 		$password=md5($_POST['password']);
